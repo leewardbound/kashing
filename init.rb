881c3f7b86d686d6aa11b52a14d907c63aaccfe1
lee <linkedlinked@gmail.com>
2011-01-28 17:52:45 -0800

    Class based kashed methods and documentation updates

--- init.rb
+++ init.rb
@@ -3,6 +3,30 @@
 #  Leverages Redis to provide flexible
 #  model kashing options
 #
+#  class RocketShip < ActiveRecord::Base
+#    # Add Kashing to existing fields
+#    kashing :title  
+#
+#    # Cached values are JSON serialized then saved into Redis, and deserialized
+#    # when retrieved. If you don't use :time => true, or specify a custom
+#    # parser/packer, then your field will be retrieved as a String
+#    kashing :launched_at, :time => true  
+#
+#    # You can even define a function, letting you add Kashing almost anywhere
+#    kashing :people_on_board do self.riders.map {|p| p.name } end 
+#
+#    # Use a custom TTL
+#    kashing :time_since_launch, :ttl => 10 do
+#      puts "Recalculating time since launch..."
+#      (Time.now - self.launched_at).to_i
+#    end
+#
+#    # If you need to do fast, class-based lookups
+#    # for cached values, disable auto-loading
+#    # and enable the class methods
+#    kashing :price, :no_auto => true, :class_method => true
+#    # Use it like: RocketShip.price_kashing(id)
+#  end
 
 module KashingPlugin
   @@redis_client = false
@@ -18,16 +42,31 @@ module KashingPlugin
   def self.included(base)
     base.send :extend, ClassMethods
   end
+
   module ClassMethods
+    def metaclass
+     class << self
+       self
+     end
+   end
+
+   # Evaluates the block in the context of the metaclass
+   def meta_eval &blk
+     metaclass.instance_eval &blk
+   end
+
     # Adds method "kashing" to models
-    def kashing(name, options = {}, &block)
+    def kashing(name, options=false, &block)
       # Share a redis connections
       mattr_accessor :redis
       @@redis ||= Redis.new
+      options ||= {}
 
       # per-model self.kashing_fields to store metadata
       class_inheritable_accessor :kashing_fields
 
+      # def Model.field_name_kashing(id) shortcut
+      class_method_shortcut(name)
 
       # attach to the class
       if !method_defined?(:kash_initialize)
@@ -51,22 +90,55 @@ module KashingPlugin
       end
       self.kashing_fields ||= {}
       
-      # Unpack the time fields
-      if options[:time] == true
-        parse = lambda {|s| Time.parse s }
-        store = lambda {|s| s.strftime '%Y-%m-%d %H:%M:%S GMT%z' }
-      end
       self.kashing_fields[name] = {
         :default_ttl => options[:ttl] || false,
-        :parse => parse || false,
-        :store => store || false,
+        :parse => options[:parse] || false,
+        :store => options[:store] || false,
         :func => func,
         :options => options
       }
     end
 
+    def class_kashing(name, options=false, &block)
+      class_inheritable_accessor :kashing_fields
+      # Share a redis connections
+      mattr_accessor :redis
+      @@redis ||= Redis.new
+      options ||= {}
+      if not block_given?
+        raise "class_kashing called without a block"
+      end
+      self.kashing_fields ||= {}
+      self.kashing_fields[name] = {
+        :default_ttl => options[:ttl] || false,
+        :parse => options[:parse] || false,
+        :store => options[:store] || false,
+        :func => block,
+        :options => options
+      }
+      meta_eval {
+        define_method "#{name}_kashing" do
+            self.smart_kash(0,name)
+        end
+        define_method "clear_#{name}" do
+            self.clear_kash(0,name)
+        end
+      }
+    end
+
     def kash_key_name(id, name)
       "#{self.name}_#{id}_#{name}"
+    end
+
+    def kash_meta(name)
+      begin
+        self.kashing_fields[name]
+      rescue
+        false
+      end
+    end
+    def kash_options(name)
+      kash_meta(name)[:options]
     end
 
     def kashed_value_for(id, name)
@@ -75,9 +147,13 @@ module KashingPlugin
       v = KashingPlugin.redis.get(kash_key_name(id, name))
       begin
         parse = self.kashing_fields[name][:parse]
-        if parse then v = parse.call(v)
-        else v = JSON.parse(v)
+        if parse then return parse.call(v) end
+        v = ActiveSupport::JSON.decode(v)
+        if 0 == (v =~ /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}/)
+          v = Time.zone.parse(v)
         end
+      rescue e
+        puts e
       ensure
         return v
       end
@@ -102,14 +178,45 @@ module KashingPlugin
         true
       end
     end
+    def set_class_kash_for(name)
+      name = name.to_sym
+      c = self.kashing_fields[name]
+      return false if not c
+      func_return = c[:func].call(self)
+      if c[:store]
+          value = c[:store].call(func_return)
+      else
+        value = ActiveSupport::JSON.encode(func_return)
+      end
+
+      KashingPlugin.redis.set(kash_key_name(0, name), value)
+      if self.kash_options(name)[:default_ttl]
+        self.clear_kash(0, name, self.kash_options(name)[:default_ttl])
+      end
+      kashed_value_for(0, name)
+    end
     def smart_kash(id, name)
       v = kashed_value_for(id, name)
-      if not v
-        c = Object.const_get(self.name).find(id)
-        v = c.set_kash_for(name)
+      if not v and not kash_options(name)[:no_auto]
+        if id != 0
+          c = Object.const_get(self.name).find(id)
+          v = c.set_kash_for(name)
+        else
+          v = set_class_kash_for(name)
+        end
       end
       v
     end
+
+
+    private
+      def class_method_shortcut(name)
+        meta_eval {
+          define_method "#{name}_kashing" do |id|
+              self.smart_kash(id,name)
+          end
+        }
+      end
   end
 
   module InstanceMethods
@@ -126,19 +233,14 @@ module KashingPlugin
       name = name.to_sym
       c = self.kashing_fields[name]
       return false if not c
+      func_return = c[:func].call(self)
       if c[:store]
-        begin
-          value = c[:store].call(c[:func].call(self))
-        rescue
-          # just return value
-        end
+          value = c[:store].call(func_return)
       else
-        value = c[:func].call(self)
-        value = value.to_json if not value.is_a? String
+        value = ActiveSupport::JSON.encode(func_return)
       end
 
       KashingPlugin.redis.set(kash_key_name(name), value)
-      c[:type] = value.class
       if self.kash_ttl(name)
         self.clear_kash(name, self.kash_ttl(name))
       end
